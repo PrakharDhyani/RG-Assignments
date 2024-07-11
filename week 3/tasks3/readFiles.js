155698f92271cd5c8e29b53995eb7e6d795f45f0
prakhar dhyani <prakhardhyani1000@gmail.com>
2024-07-11 11:33:14 +0530

    Displaying transactions.json content

--- week 3/tasks3/readFiles.js
+++ week 3/tasks3/readFiles.js
@@ -4,9 +4,14 @@ const path = require('path');
 async function displayTransactions() {
   try {
     // TODO: Read 'transactions.json' file using fs.readFile or fs.readFile with async/await
+    const filePath = path.join("data", 'transactions.json');
+    // console.log(filePath)
+    const fileData = await fs.readFile(filePath,"utf-8");
+    console.log(fileData)
    
   } catch (error) {
     // TODO: Handle errors that occur during file reading or JSON parsing
+    console.log(error)
   }
 }
 
